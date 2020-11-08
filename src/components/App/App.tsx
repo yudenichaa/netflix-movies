509ee438066f1307fa3483c7c4937b2425679998
yudenichaa <yudenichalexandr@yandex.ru>
2020-11-08 17:11:53 +0300

    add interface to App component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,7 +5,9 @@ import { movieCategories } from "../../moviesData";
 import netflixLogo from "./assets/netflix.svg";
 import "./app.scss";
 
-export default function () {
+interface IApp {}
+
+const App: React.FC<IApp> = () => {
     return (
         <div className="app">
             <img className="app__logo" src={netflixLogo} alt="logo" />
@@ -20,4 +22,6 @@ export default function () {
             ))}
         </div>
     );
-}
+};
+
+export default App;
